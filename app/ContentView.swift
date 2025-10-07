f0801cd6c33a3c6bd160d5ac23d3c444f7a137f5
jurre111 <jurredijkstra@mijnheem.nl>
2025-10-07 23:52:18 +0200

    Update ContentView.swift

--- app/ContentView.swift
+++ app/ContentView.swift
@@ -1,27 +1,9 @@
 import SwiftUI
-import Charts
 
 struct ContentView: View {
-    @State private var slope = 1.0
-    @State private var isEditing = false
 
     var body: some View {
-        Chart {
-            LinePlot(x: "x", y: "y") { x in
-                slope*x
-            }
-        }
-        .chartXScale(domain: -10...10)
-        .chartYScale(domain: -10...10)
-        .frame(height: 200)
-        .padding()
-        Slider(
-            value: $slope,
-            in: 0...3,
-            onEditingChanged: { editing in
-                isEditing = editing
-            }
-        )
+        Text("Hello World!")
     }
 }
 
